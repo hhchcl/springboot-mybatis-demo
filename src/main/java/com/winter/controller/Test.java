84ab75b00e19fd2371ce59b25400591b43dfcc27
hc <1245035689@qq.com>
2019-04-25 15:51:44 +0800

    #main02

--- src/main/java/com/winter/controller/Test.java
+++ src/main/java/com/winter/controller/Test.java
@@ -6,5 +6,6 @@ package com.winter.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test分支");
+        System.out.println("test分支02");
     }
 }
