975da9769b9a1f220897a7ad216b87d484531d01
hc <1245035689@qq.com>
2019-04-25 12:04:07 +0800

    #测试mybatis与springboot

---
+++ src/main/java/com/winter/model/User.java
@@ -0,0 +1,43 @@
+package com.winter.model;
+
+public class User {
+    private Integer user_id;
+
+    private String user_name;
+
+    private String passowrd;
+
+    private String phone;
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name == null ? null : user_name.trim();
+    }
+
+    public String getPassowrd() {
+        return passowrd;
+    }
+
+    public void setPassowrd(String passowrd) {
+        this.passowrd = passowrd == null ? null : passowrd.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
